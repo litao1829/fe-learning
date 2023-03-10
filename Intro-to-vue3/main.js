668fe605402ecfcb97daea637edfd75aa2ef0410
litao <2532566074@qq.com>
2023-03-10 20:11:20 +0800

    事件处理

--- Intro-to-vue3/main.js
+++ Intro-to-vue3/main.js
@@ -15,17 +15,24 @@ const app=Vue.createApp({
             details:['50% cotton','30% wool','20% polyester'],
             size:['S','M','L','XL'],
             variants:[
-                {id:2234,color:'green'},
-                {id:2235,color:'blue'}
+                {id:2234,color:'green',image:'./assets/images/socks_green.jpg'},
+                {id:2235,color:'blue',image:'./assets/images/socks_blue.jpg'}
             ],
+            cart:0,
         }
     },
     methods:{
-
+        addToCart(){
+            this.cart+=1;
+        },
+        updateImage(image){
+            this.image=image;
+        },
+        minusToCart(){
+            if(this.cart>0){
+                this.cart-=1;
+            }
+        }
     },
-    attri()
-    {
-        return this.attri;
-    }
 });
 
