d2cb71bca919834807bd2b86a920400321a64e7f
litao <2532566074@qq.com>
2023-03-10 09:43:16 +0800

    属性绑定

---
+++ Intro-to-vue3/main.js
@@ -0,0 +1,11 @@
+const app=Vue.createApp({
+    data(){
+        return {
+            product:'Socks',
+            description:'A warm fuzzy pair of socks.',
+            image:'./assets/images/socks_blue.jpg',
+            url:'https://cn.vuejs.org/',
+            inStock:true
+        }
+    }
+});
